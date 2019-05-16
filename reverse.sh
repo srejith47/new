e8afa3d6e6a70123dc53cd9240fbd7a3a86b9d54
adwaithpn <nealpn78@gmail.com>
2019-05-16 16:27:31 +0530

    the commit

---
+++ reverse.sh
@@ -0,0 +1,12 @@
+
+echo “Enter the number”
+read n
+sd=0
+rev=0
+while [ $n -gt 0 ]; do
+sd=$(( $n % 10 ))
+rev=$(( $rev * 10 + $sd ))
+n=$(( $n / 10 ))
+done
+echo "Reverse of the number is "$rev
+
